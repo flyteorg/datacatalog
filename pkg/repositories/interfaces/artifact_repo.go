a8d9887f7a0aaf6e35bf66a43b398eb631c3d2b0
Andrew Chan <achan@lyft.com>
2019-11-12 17:52:44 -0800

    clean up

--- pkg/repositories/interfaces/artifact_repo.go
+++ pkg/repositories/interfaces/artifact_repo.go
@@ -11,14 +11,3 @@ type ArtifactRepo interface {
 	Get(ctx context.Context, in models.ArtifactKey) (models.Artifact, error)
 	List(ctx context.Context, datasetKey models.DatasetKey, in models.ListModelsInput) ([]models.Artifact, error)
 }
-
-// input:
-// FilterExpression
-// output:
-// ArtifactFilter
-// - this will be passed into the repo to construct the GORM expression
-// -
-// in GORM land we want to
-// - construct a Tx and apply the
-// - JOIN: based on the different entity filters
-// - WHERE's: based on the different entity filters
