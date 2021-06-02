8e1b319a04c8259c89061b7f653a987491875515
Chao-Han Tsai <milton0825@gmail.com>
2021-06-01 17:52:24 -0700

    add more comments
    Signed-off-by: Chao-Han Tsai <milton0825@gmail.com>
    Signed-off-by: Daniel Rammer <daniel@union.ai>

--- pkg/manager/interfaces/reservation.go
+++ pkg/manager/interfaces/reservation.go
@@ -6,6 +6,9 @@ import (
 	"github.com/flyteorg/flyteidl/gen/pb-go/flyteidl/datacatalog"
 )
 
+// ReservationManager is the interface to handle reservation requests.
+// You can find more details about the APIs in datacatalog service proto
+// in flyteidl
 type ReservationManager interface {
 	GetOrReserveArtifact(context.Context, *datacatalog.GetOrReserveArtifactRequest) (*datacatalog.GetOrReserveArtifactResponse, error)
 	ExtendReservation(context.Context, *datacatalog.ExtendReservationRequest) (*datacatalog.ExtendReservationResponse, error)
--- pkg/repositories/interfaces/reservation_repo.go
+++ pkg/repositories/interfaces/reservation_repo.go
@@ -7,7 +7,13 @@ import (
 	"github.com/flyteorg/datacatalog/pkg/repositories/models"
 )
 
+// Interface to interact with Reservation Table
 type ReservationRepo interface {
+
+	// Get reservation
 	Get(ctx context.Context, reservationKey models.ReservationKey) (models.Reservation, error)
+
+	// Create the reservation. If the reservation already exists, we try to take over the
+	// reservation via update.
 	CreateOrUpdate(ctx context.Context, reservation models.Reservation, now time.Time) error
 }
