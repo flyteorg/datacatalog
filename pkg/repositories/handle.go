92666efd58165970235ba97f6e03d1424b12b2d5
Daniel Rammer <daniel@union.ai>
2021-11-03 11:40:19 -0500

    capturing create extension result and failing if it is an error
    Signed-off-by: Daniel Rammer <daniel@union.ai>

--- pkg/repositories/handle.go
+++ pkg/repositories/handle.go
@@ -71,8 +71,12 @@ func (h *DBHandle) CreateDB(dbName string) error {
 func (h *DBHandle) Migrate(ctx context.Context) error {
 	if h.db.Config.Dialector.Name() == config.Postgres {
 		logger.Infof(ctx, "Creating postgres extension uuid-ossp if it does not exist")
-		h.db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
+		result := h.db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
+		if result.Error != nil {
+			return result.Error
+		}
 	}
+
 	if err := h.db.AutoMigrate(&models.Dataset{}); err != nil {
 		return err
 	}
